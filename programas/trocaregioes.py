208af4d0b94d732dc22bc9536a475f4563c34323
Andres <andresaar94@gmail.com>
2017-09-12 16:22:49 -0300

    mais

--- programas/trocaregioes.py
+++ programas/trocaregioes.py
@@ -3,8 +3,9 @@ import numpy as np
 
 image = cv2.imread("nina.jpg",0)
 cv2.imshow("image",image)
-cv2.waitKey(0)
 height, width = image.shape
+cv2.waitKey(0)
+
 
 [a,b] = np.split(image,[int(height/2)], 0)
 image = np.concatenate((b,a))
